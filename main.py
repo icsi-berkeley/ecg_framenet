cfece99c38f63b370d116641711caf7915f2ae3f
Sean Trott <seantrott@icsi.berkeley.edu>
2016-03-21 10:35:00 -0700

    Added hypothesis cxns, and a new demo for main.py

--- main.py
+++ main.py
@@ -27,16 +27,22 @@ def main(data_path):
 if __name__ == "__main__":
 	fn, fnb = main(sys.argv[1])
 
-	"""
-	DEMO:
+	
+
+	# DEMO: SCHEMAS
+	schemas = utils.generate_schemas_for_frames(fn.frames)
+	# Write these to a file
+
+
+	# DEMO: CONSTRUCTIONS
 	total = build_cxns_for_frame("Motion", fn, fnb, "Manner", "V")
 
-	You can then write the values from the total dictionary into files:
-	* cxns_all: all valences converted 1-1 to cxns 
-	* cxns_collapsed: valences collapsed into smaller set 
-	* tokens: tokens created from these frames ("swarm.v", etc.)
-	* types: type-cxns ("Fluidic_motionType") created from these frames 
-	"""
+	#You can then write the values from the total dictionary into files:
+	#* cxns_all: all valences converted 1-1 to cxns 
+	#* cxns_collapsed: valences collapsed into smaller set 
+	#* tokens: tokens created from these frames ("swarm.v", etc.)
+	#* types: type-cxns ("Fluidic_motionType") created from these frames 
+	
 
 
 
