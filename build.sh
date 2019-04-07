6a24d976b08a49443855ec36f63c5351169f4fa7
Nikhil Bishnoi <nikhil.bishnoi@research.iiit.ac.in>
2019-04-08 00:37:09 +0530

    Build.sh changed for both 1.6 and 1.7 version
    Now you can use it for both 1.6 and 1.7 version

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # If necessary, replace the paths below with the full-paths on your computer.
-python3 -i main.py fndata-1.6/
+if [ -f fndata-1.6 ]; then
+   python3 -i main.py fndata-1.6/   
+else
+   python3 -i main.py fndata-1.7/
+fi
